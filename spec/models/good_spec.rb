5b26cfee1778b07b293f826b70e949868c8a247d
Kingsley Wang <kwang@multiservice.com>
2019-04-25 12:26:59 +1000

    Addes specs to check entry_at and exit_at

--- spec/models/good_spec.rb
+++ spec/models/good_spec.rb
@@ -29,4 +29,18 @@ RSpec.describe Good, type: :model do
     another_good = Good.create(consignment_id: 'DP1001AP', name: 'Coconuts', type: 'Food')
     expect(another_good.invalid?).to eq true
   end
+
+  it 'create entry_at for new consignment id' do
+    good = Good.create(consignment_id: 'AS1001AP', name: 'Milk', type: 'Food')
+    another_good = Good.create(consignment_id: 'DP1001AP', name: 'Coconuts', type: 'Food')
+    expect(good.entry_at).not_to be_nil
+    expect(another_good.entry_at).not_to be_nil
+  end
+
+  it 'create exit_at for exist consignment id' do
+    good = Good.create(consignment_id: 'AS1001AP', name: 'Milk', type: 'Food')
+    another_good = Good.create(consignment_id: 'AS1001AP', name: 'Coconuts', type: 'Food')
+    expect(good.exit_at).to be_nil
+    expect(another_good.exit_at).not_to be_nil
+  end
 end
