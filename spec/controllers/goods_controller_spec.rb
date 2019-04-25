0f7628d75eb78c9e01141ffcb398c6c679834780
Kingsley Wang <kwang@multiservice.com>
2019-04-25 22:55:52 +1000

    Added specs for filter

--- spec/controllers/goods_controller_spec.rb
+++ spec/controllers/goods_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe GoodsController, type: :controller do
+  let!(:good1) do
+    Good.create(consignment_id: 'AS1001AP', name: 'TEST', type: 'test')
+  end
 
+  let!(:good2) do
+    Good.create(consignment_id: 'DS1001AP', name: 'ABC', type: 'Liquid')
+  end
+
+  it 'index' do
+    get :index
+    expect(assigns[:goods].size).to eq 2
+  end
+
+  it 'filter by type' do
+    get :index, params: { type: 'test' }
+    expect(assigns[:goods].size).to eq 1
+  end
+
+  it 'filter by entry_at' do
+    get :index, params: { entry_at: Time.now }
+    expect(assigns[:goods].size).to eq 2
+  end
 end
