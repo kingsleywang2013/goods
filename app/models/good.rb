2bca211c765242055d98c3fe7376f51171b921eb
Kingsley Wang <kwang@multiservice.com>
2019-04-25 13:39:54 +1000

    Implemented before_save check for entry_at and exit_at

--- app/models/good.rb
+++ app/models/good.rb
@@ -3,4 +3,16 @@ class Good < ApplicationRecord
 
   validates :consignment_id, :name, :type, presence: true
   validates :name, uniqueness: true
+
+  before_save :check_consignment_id
+
+  private
+
+  def check_consignment_id
+    if Good.find_by(consignment_id: self.consignment_id).nil?
+      self.entry_at = DateTime.now
+    else
+      self.exit_at = DateTime.now
+    end
+  end
 end
