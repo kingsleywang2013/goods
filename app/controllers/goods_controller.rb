63054b85c101e7e8a3a9f5fdb270993b31046a68
Kingsley Wang <kwang@multiservice.com>
2019-04-25 21:30:20 +1000

    Added filter

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,7 +1,15 @@
 class GoodsController < ApplicationController
   before_action :set_import, only: %i(new import)
   def index
-    @goods = Good.all
+    if params[:type].present?
+      @goods = Good.filter_by_type(params[:type])
+    elsif params[:entry_at].present?
+      @goods = Good.filter_by_entry_at(params[:entry_at])
+    elsif params[:type].present? && params[:entry_at].present?
+      @goods = Good.mix_filter(params[:type], params[:entry_at])
+    else
+      @goods = Good.all
+    end
   end
 
   def import
--- app/models/good.rb
+++ app/models/good.rb
@@ -1,6 +1,11 @@
 class Good < ApplicationRecord
   self.inheritance_column = nil
 
+  scope :filter_by_type, ->(type) { where('type LIKE ?', "%#{type}%") }
+  scope :filter_by_entry_at, ->(entry) { where(entry_at: entry.to_date.beginning_of_day..entry.to_date.end_of_day) }
+  scope :mix_filter, ->(type, entry) { where('type LIKE ?', "%#{type}%").where(entry_at: entry.to_date.beginning_of_day..entry.to_date.end_of_day) }
+
+
   validates :consignment_id, :name, :type, presence: true
   validates :name, uniqueness: true
 
